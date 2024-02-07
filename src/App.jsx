c00949cda05746c3c4b5733ffb8548699ce35516
FahimaGold <df_mokhtari@esi.dz>
2024-02-07 11:29:18 +0100

    remove unnecessary code from App

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,10 @@ import RecordDetailsComponent from './components/RecordDetailsComponent';
 import './App.css'
 
 function App() {
-  const accessToken = useAccessToken();
-  const {candidacyRecord, fetchCandidacyRecord} = useCandidacyRecord(accessToken);
-  const handleRecordClick = ()=>{
-    
-    fetchCandidacyRecord();
-  }
+ 
   return (
     <> 
       <div className="card">
-        <p>{accessToken}</p>
         <span>
         record ID a004L000002gCJK
           </span>
